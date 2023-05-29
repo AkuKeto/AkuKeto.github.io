a48e68db2b077cb2c4d7fc31c076e3d8bbab17ae
AkuKeto <133851620+AkuKeto@users.noreply.github.com>
2023-05-29 09:05:14 +0300

    iso päivitys

--- demo8/kirjautuminen.js
+++ demo8/kirjautuminen.js
@@ -3,11 +3,24 @@ document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 function onkoKirjautunut() {
     if (localStorage.getItem("kirjautunut") === "kylla") {
         document.getElementById("tervetulo_teksti").textContent += localStorage.getItem("nimi");
-        document.getElementById("kirjautumis_lomake").style.display = "none";
+        document.getElementById("kirjaudu_ulos_lomake").style.display;
+        document.getElementById("kirjautumis_lomake").style.display="none"
+    }
+}
+
+function onkoKirjautunut() {
+    if (localStorage.getItem("kirjautunut") === "ei") {
+        document.getElementById("kirjaudu_teksti").textContent;
+        document.getElementById("kirjaudu_ulos_lomake").style.display="none";
+        document.getElementById("kirjautumis_lomake").style.display;
     }
 }
 
 function kirjaudu() {
     localStorage.setItem("nimi", document.getElementById("nimi").value);
     localStorage.setItem("kirjautunut", "kylla");
+}
+function kirjaudu_ulos() {
+    localStorage.setItem("nimi", value="");
+    localStorage.setItem("kirjautunut", "ei");
 }
